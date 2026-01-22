d6ac9c47d6d52e648f330842398701d95e5d2baa
Aniketdubey78 <aniketdubey17897@gmail.com>
2026-01-22 20:16:44 +0530

    initial commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
     }
     return (
          <>
-          <form onSubmit={submitHandler} className="max-w-md mx-auto  pt-4 border border-2 p-8 bg-orange-200" >
+          <form onSubmit={submitHandler} className="max-w-md mx-auto  pt-4  border-2 p-8 bg-orange-200" >
       <div className="mb-5">
         <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
         <input onChange={changeHandler} name="name" type="text" id="name" className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="Salman Khan" required />
